092845797123d154b88157da818977082e4d1367
khomenkovetal <khomenkovetal@gmail.com>
2021-06-04 14:29:01 +0300

    Створили конструктори

--- Tetris/Point.cs
+++ Tetris/Point.cs
@@ -9,10 +9,21 @@ namespace Tetris
         public int x;
         public int y;
         public char c;
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
         }
+
+        public Point(int a, int b, char sym)
+        {
+            x = a;
+            y = b;
+            c = sym;
+        }
+        public Point() { }       
+
+
     }
 }
