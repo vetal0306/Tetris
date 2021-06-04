aa64e3cc5c07a07f54894b353c3f4b8a112e9d76
khomenkovetal <khomenkovetal@gmail.com>
2021-06-04 13:55:23 +0300

    Виводимо дві точки

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -6,7 +6,23 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+            Draw(x1, y1, c1);
+
+            int x2 = 3;
+            int y2 = 4;
+            char c2 = '#';
+            Draw(x2, y2, c2);
+            
+        }
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
