75da1e1dd226373f765bd37b024e64ecb08f4fa0
khomenkovetal <khomenkovetal@gmail.com>
2021-06-04 14:17:54 +0300

    Доданий клас Point

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -8,21 +8,20 @@ namespace Tetris
         {
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
-            int x1 = 2;
-            int y1 = 3;
-            char c1 = '*';
-            Draw(x1, y1, c1);
-
-            int x2 = 3;
-            int y2 = 4;
-            char c2 = '#';
-            Draw(x2, y2, c2);
             
-        }
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
-        }
+
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 6;
+            p2.y = 6;
+            p2.c = '#';
+            p2.Draw();
+
+        }        
     }
 }
