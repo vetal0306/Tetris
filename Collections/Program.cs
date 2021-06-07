9e5b41023f2b53c414380f09ad47a01c6e28f93e
khomenkovetal <khomenkovetal@gmail.com>
2021-06-07 10:28:51 +0300

    створили клас Square

---
+++ Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Tetris;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = new int[5];
+            num1[0] = 1;
+            num1[2] = 6;
+
+            
+            //foreach(int i in num1)
+            //{
+            //   Console.WriteLine(i);
+            //}
+
+            Point[] points = new Point[3];
+            points[0] = new Point(1, 2, '#');
+            points[1] = new Point(4, 2, '#');
+            points[2] = new Point(1, 3, '#');
+
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -8,19 +8,21 @@ namespace Tetris
         {
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
-            
 
-            Point p1 = new Point(2, 3, '*');            
-            p1.Draw();
+            Square s = new Square(2, 5, '*');
+            s.Draw();
 
-            Point p2 = new Point()
-            {
-                x = 4,
-                y = 5,
-                c = '#'
-            };
+            //Point p1 = new Point(2, 3, '*');            
+            //p1.Draw();
+
+            //Point p2 = new Point()
+            //{
+              //  x = 4,
+               // y = 5,
+               //c = '#'
+            //};
             
-            p2.Draw();
+            //p2.Draw();
 
         }        
     }
