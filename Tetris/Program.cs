48a04873108d906247d88301780dee64cbbef0b1
khomenkovetal <khomenkovetal@gmail.com>
2021-06-08 10:57:00 +0300

    реалізували падіння фігури в нескінченному циклі

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -20,45 +20,25 @@ namespace Tetris
             #endregion
 
             FigureGenerator generator = new FigureGenerator(20, 0, '*');
-            Figure s = generator.GetNewFigure(); //квадрат з точок '*'
-            s.Draw();
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Rotate();
-            s.Draw();           
-
-            Thread.Sleep(1000);
-            s.Hide();
+            Figure s = null;
+            while (true)
+            {
+                FigureFall(s, generator);
+            }          
             
-            s.Move(Direction.DOWN);
-            s.Draw();
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Rotate();
-            s.Move(Direction.DOWN);
-            s.Draw();
+        }
+        static void FigureFall(Figure fig, FigureGenerator generator)
+        {
+            fig = generator.GetNewFigure();
+            fig.Draw();
 
-            Thread.Sleep(1000);
-            s.Hide();
-            s.Rotate();
-            s.Move(Direction.LEFT);
-            s.Draw();
-
-            //Stick k = new Stick(4, 7, '$'); // полоска з символів '$'
-            //k.Draw();
-
-            //Point p1 = new Point(2, 3, '*');            
-            //p1.Draw();
-
-            //Point p2 = new Point()
-            //{
-            //  x = 4,
-            // y = 5,
-            //c = '#'
-            //};
-
-            //p2.Draw();
-
+            for (int i = 0; i < 15; i++)
+            {
+                fig.Hide();
+                fig.Move(Direction.DOWN);
+                fig.Draw();
+                Thread.Sleep(1000);
+            }
         }
     }
 }
